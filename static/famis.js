8ac15f2983b101a782e212bea291f7bb7e6862fd
Emma Smithayer <esmithayer@gmail.com>
2015-01-14 17:02:59 -0800

    Added a search form that prints some data when submitted.

--- static/famis.js
+++ static/famis.js
@@ -1,15 +1,32 @@
+var allData;
+
 function init() {
   Tabletop.init({
     key: '1cNaJmqIIJvWi1OrLSAFPQpddC0uau6nWnLdhyB2FSyU',
-    callback: buildFamis,
+    callback: function(data) {allData = data;},
     simpleSheet: true
   });
 }
 
-function buildFamis(data) {
-  for (var d = 0; d < data.length; d++) {
-    console.log(data[d]);
-  }
+function buildFamis(e) {
+    printSearchResult(allData[0]);
+}
+
+function printSearchResult(result) {
+    var coveredText;
+    if (result.covered_yn == 'Yes') {
+	coveredText = "FAMIS covers ";
+    }
+    else {
+	coveredText = "FAMIS does not cover ";
+    }
+    coveredText = coveredText.concat(result.service);
+    $(".covered").append(coveredText);
+
+    var copayText = "Copay: ".concat(result.copay_below_150);
+    $(".copay").append(copayText)
+    var descriptionText = result.description_new;
+    $(".description").append(descriptionText);
 }
 
 window.onload = init;
