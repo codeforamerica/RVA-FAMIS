0e62b10f6dd5dff6498652c5ebec7906034bde1c
Benjamin Golder <benjamin.j.golder@gmail.com>
2015-01-16 10:31:31 -0800

    added basic trappings of django project

---
+++ backend/backend/settings.py
@@ -0,0 +1,53 @@
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+REPO_DIR = os.path.dirname(BASE_DIR)
+
+
+if os.environ.get('IS_DEV'):
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
+
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'backend.urls'
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
